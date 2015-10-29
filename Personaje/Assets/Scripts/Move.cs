4955445bca6b419d620b93bb88ec435483f9cca9
HugoIP <hugo@amsmedia.com>
2015-10-29 15:03:42 -0600

    Personaje se mueve a iz y der

---
+++ Personaje/Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//moveForward ();
+	}
+	//funcion que mueve al personaje
+	public void moveLe()
+	{
+		//Debug.Log ("Hola ");
+		//Mover posicion de forma constante
+		//Vector3.down
+		this.transform.Translate( (Time.deltaTime * speed) * Vector3.left);
+	}
+	//funcion que mueve al personaje
+	public void moveR()
+	{
+		//Debug.Log ("Hola ");
+		//Mover posicion de forma constante
+		this.transform.Translate( (Time.deltaTime * speed) * Vector3.right);
+	}
+}
