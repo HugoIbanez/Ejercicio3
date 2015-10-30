b12ae5b437ec1189ee0e9ab83d0b78ead4d4d815
LC02-01 <LC02-01>
2015-10-30 14:22:45 -0600

    Vesion juego

--- Personaje/Assets/Scripts/Emisor.cs
+++ Personaje/Assets/Scripts/Emisor.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class Emisor : MonoBehaviour {
 	public GameObject objetoPrefab;
+	public int total;
+	public float counter;
+	public float inter;
 	// Use this for initialization
 	void Start () {
-	
+		total = 20;
+		counter = 0;
+		inter = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (counter > inter) 
+		{
+			counter=0;
+			secuenciaRandom();
+		}
+		counter += Time.deltaTime;
+		//float pos;
+		//pos= Random.Range(-10,10);
+		//Vector3 newPos = new Vector3(this.transform.position.x+pos,this.transform.position.y,this.transform.position.z);
 
-		float pos;
-		pos= Random.Range(-10,10);
-		Vector3 newPos = new Vector3(this.transform.position.x+pos,
-			this.transform.position.y,
-			this.transform.position.z);
-
-		Instantiate(objetoPrefab,newPos,Quaternion.identity);
+	}
+	public void secuenciaRandom()
+	{
+		int initRange = (total/2)-total;
+		for (int i=0; i<total; i++) 
+		{
+			if(Random.Range(0,2)==1)
+			{
+				Instantiate(objetoPrefab,new Vector3(this.transform.position.x+(initRange+i),this.transform.position.y,this.transform.position.z),Quaternion.identity);
+			}
+		}
 	}
 }
